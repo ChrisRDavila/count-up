a7599904441db73e98f0be61a641bb40f00e9ed9
ChrisRDavila <crossdavila@gmail.com>
2023-03-29 10:30:04 -0700

    test 2 adds by 5

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//function loopdaloop(number){
+//  for(let i=0; i < number; i++) {
+//    console.log(i);
+//  }
+//}
+
+function loopdaloop(number){
+  let answer;
+  for(let i=0; i <= number; i++) {
+    answer = number * 5;
+    console.log(i*5)
+  }
+}
